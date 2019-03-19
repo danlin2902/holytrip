7fc6d077e3278b9f2cbc98d9fc2a4eea5901869f
andriy kravets <4kravets@gmail.com>
2019-03-19 15:02:59 +0100

    trips and participants controllers

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,2 +1,24 @@
 class ParticipantsController < ApplicationController
+
+  def new
+    @trip = Trip.find(params[:id])
+    @participant = Participant.new
+  end
+
+  def create
+    @participant.trip = Trip.find(params[:trip_id])
+    @participant.user = User.find(params[:user_id])
+    @participant = Participant.new(participant_params)
+    if @participant.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def participant_params
+    params.require(:participant).permit(:status)
+  end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,47 @@
 class TripsController < ApplicationController
+  before_action :find_trips, only: [ :show, :edit, :destroy, :update ]
+
+  def index
+    @trips = Trip.where(user: current_user)
+  end
+
+  def show
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user = current_user
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @trip.update(trip_params)
+    redirect_to trip_path(@trip)
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :destination, :start_date, :end_date, :photo)
+  end
+
+  def find_trips
+    @trip = Trip.find(params[:id])
+  end
 end
