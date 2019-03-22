45029fcc34333a8c6a1b5d2c2762a31ab67506ef
Regi Voda <vodaregi@gmail.com>
2019-03-22 14:26:38 +0100

    task manager almost done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,13 @@ import "bootstrap";
 import "../plugins/flatpickr"
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
+import {MDCRipple} from '@material/ripple';
+
+
 import { initMapbox } from '../plugins/init_mapbox';
 import { initPlaces } from '../plugins/init_places';
 
 initMapbox();
 initsearchAlgoliaPlaces();
 
+const listItemRipples = list.listElements.map((listItemEl) => new MDCRipple(listItemEl));
