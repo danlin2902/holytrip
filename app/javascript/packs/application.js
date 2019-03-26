72d761d5f90daebf61c4e46c061f39b5b1e4509a
Regi Voda <vodaregi@gmail.com>
2019-03-26 17:20:05 +0100

    FUCKINGDRAGGABLESHITTYTHING

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
-import "../plugins/flatpickr"
+import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-
+import "../plugins/sortable.js";
 import { initMapbox } from '../plugins/init_mapbox';
 import { searchAlgoliaPlaces } from '../plugins/init_places';
 
